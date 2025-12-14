8da292f22dacde38c91968f279e745ad2a745b86
Teresa <35701568+TeresaPegan@users.noreply.github.com>
2025-12-14 14:48:52 -0500

    Update Continuity_Anchor_Test_PART2.py
    similar to commit with PART1.py script, this has updates to allow individuals to come from a multi-individual VCF and to improve some of how paths are encoded for more general use. I implemented and debugged these changes with input from ChatGPT 5.1

--- Anchor_test/Continuity_Anchor_Test_PART2.py
+++ Anchor_test/Continuity_Anchor_Test_PART2.py
@@ -11,65 +11,95 @@ import get_file_name
 import random
 
 ################################ Functions ###################################
-def check_if_missingness(full_info):
-    a_list=full_info[8:]
-    [info,x]=a_list
-    d=x.split(':')
-    if d[0]=='./.':
+def check_if_missingness(format_str, sample_str):
+    d = sample_str.split(':')
+    gt = d[0]
+    if gt in ['./.', '.|.']:
         return 1
+    return 0
+
+def get_haploid_genotype(ref_nt, alt_nt, format_str, sample_str):
+    """
+    Extract haploid genotype call and coverage for a single sample in a multi-individual VCF.
+
+    - ref_nt, alt_nt: reference and (single) alternate allele from VCF
+    - format_str: FORMAT column (e.g., "GT:AD:DP:GQ:PL" or "GT:DP:A:C:G:T:PP:GQ")
+    - sample_str: sample's genotype field corresponding to format_str
+
+    Returns [coverage, haploid_nt] where haploid_nt is one of ref_nt / alt_nt / ''.
+    """
+
+    fmt_fields = format_str.split(':')
+    d = sample_str.split(':')
+
+    # Missing genotype
+    gt = d[0]
+    if gt in ['./.', '.|.']:
+        return [0, '']
+
+    # Build map FORMAT field -> index
+    fmt_idx = {name: i for i, name in enumerate(fmt_fields)}
+
+    # Get coverage (DP if present, else from AD, else 0)
+    coverage = 0
+    if 'DP' in fmt_idx and fmt_idx['DP'] < len(d) and d[fmt_idx['DP']] not in ['.', '']:
+        coverage = int(d[fmt_idx['DP']])
+    elif 'AD' in fmt_idx and fmt_idx['AD'] < len(d) and d[fmt_idx['AD']] not in ['.', '']:
+        # Sum AD if DP missing
+        ad_vals = d[fmt_idx['AD']].split(',')
+        coverage = sum(int(x) for x in ad_vals if x not in ['.', ''])
     else:
-        return 0
+        coverage = 0
 
-def get_haploid_genotype(full_info):
-    ref_nt=full_info[3]
-    alt_nt=full_info[4]
-    a_list=full_info[8:]
-    b_geno=''
-    coverage=0
-    [info,x]=a_list
-    d=x.split(':')
-    if len(d)>5: # a snpAD vcf has "GT:DP:A:C:G:T:PP:GQ" in sample field whether homozygote or heterozygote
-        coverage=int(d[1])
-        if d[0]=='0/0': # site is homozygote reference
-            return [coverage,ref_nt]
-        elif d[0]=='1/1':
-            return [coverage,alt_nt]
-        else: # you have a heterozygote
-            nt_pos={'A':2,'C':3,'G':4,'T':5} # dict to index nucleotide coverages in list
-            ref_cov = sum(int(x) for x in d[nt_pos[ref_nt]] if x.isdigit()) # sum reads supporting reference allele
-            if len(alt_nt)==1: # you have only a single alt allele
-                alt_cov = sum(int(x) for x in d[nt_pos[alt_nt]] if x.isdigit())
-                return [coverage,random.choice([ref_nt,alt_nt])]
-            else:	#You have >1 alt allele; multiallelic sites should have been filtered before
-                print('need to be worried?',info,x,full_info[1])
-                input()
+    # Case 1: SNPAD-style VCF with per-nucleotide fields "A","C","G","T"
+    if all(x in fmt_idx for x in ['A', 'C', 'G', 'T']):
+        # Extract per-nt coverages by letter
+        nt_pos = {nt: fmt_idx[nt] for nt in ['A', 'C', 'G', 'T']}
+        ref_cov = int(d[nt_pos[ref_nt]]) if ref_nt in nt_pos and d[nt_pos[ref_nt]].isdigit() else 0
 
-    else:  # it is a GATK called vcf with "GT:DP" (homozygote or missing), or "GT:AD:DP:GQ:PL" (heterozygote) in sample field
-        if not info=='GT:AD:DP:GQ:PL':  #This depends on GATK pipeline used
-            if info=='GT:DP' and d[0]=='0/0':
-                return [int(d[1]),ref_nt]
-            elif info=='GT':
-                return [0,'']
-            else:
-                print('need to be worried?',info,x,full_info[1])
-                input()
-        else:   # You have >=1 alt allele
-            temp=d[1].split(',') # split the AD field to get covs of ref and alt nucleotides
-            if len(temp)==2:
-                [ref_cov,alt_cov]=temp
-                ref_cov=int(ref_cov)
-                alt_cov=int(alt_cov)
-                coverage=int(d[2])
-                if ref_cov==0:
-                    return [coverage,alt_nt]
-                elif alt_cov==0:
-                    return [coverage,ref_nt]
-                else:
-                    return [coverage,random.choice([ref_nt,alt_nt])]
+        if len(alt_nt) == 1 and alt_nt in nt_pos and d[nt_pos[alt_nt]].isdigit():
+            alt_cov = int(d[nt_pos[alt_nt]])
+        else:
+            # multiallelic or unexpected; skip
+            return [0, '']
 
-            else:	#You have >1 alt allele; multiallelic sites should have been filtered before
-                print('need to be worried?',info,x,full_info[1])
-                input()
+        if ref_cov == 0 and alt_cov == 0:
+            return [coverage, '']
+        if ref_cov == 0:
+            return [coverage, alt_nt]
+        if alt_cov == 0:
+            return [coverage, ref_nt]
+        # Both present: randomly choose one
+        return [coverage, random.choice([ref_nt, alt_nt])]
+
+    # Case 2: GATK-like with AD field ("GT:AD:DP:GQ:PL" or similar)
+    if 'AD' in fmt_idx and fmt_idx['AD'] < len(d):
+        temp = d[fmt_idx['AD']].split(',')
+        if len(temp) >= 2:
+            ref_cov = int(temp[0]) if temp[0] not in ['.', ''] else 0
+            alt_cov = int(temp[1]) if temp[1] not in ['.', ''] else 0
+
+            if coverage == 0 and (ref_cov + alt_cov) > 0:
+                coverage = ref_cov + alt_cov
+
+            if ref_cov == 0 and alt_cov == 0:
+                return [coverage, '']
+            if ref_cov == 0:
+                return [coverage, alt_nt]
+            if alt_cov == 0:
+                return [coverage, ref_nt]
+            return [coverage, random.choice([ref_nt, alt_nt])]
+
+    # Fallback: interpret genotype directly when AD info is not usable
+    if gt in ['0/0', '0|0']:
+        return [coverage, ref_nt]
+    elif gt in ['1/1', '1|1']:
+        return [coverage, alt_nt]
+    elif gt in ['0/1', '1/0', '0|1', '1|0']:
+        return [coverage, random.choice([ref_nt, alt_nt])]
+    else:
+        return [0, '']
+
 
 
 def orient_and_get_count_haploid(haploid_gt,anc_nt,der_nt):
@@ -117,36 +147,27 @@ def make_out_str(a_list):
 
 # Comparing recent individuals' vcfs against the HETPOS file created above
 
-hetpath='/domus/h1/anchor_method/DIR_anchorind_hetcounts_perchr'
-vcf_path='/crex/proj/snic2020/private/vcf_files/PER_CHR_VCFS'
-
 #Throw an error unless you have at least two arguments to command line
-if len(sys.argv)<4:
-    sys.exit('Input Error: The Anchor continuity test PART2 requires a command line input of format: python script.py the_chr anchor_ind_vcf recent_ind_vcf')
+if len(sys.argv)<6:
+    sys.exit('Input Error: The Anchor continuity test PART2 requires a command line input of format: python script.py the_chr anchor_ind recent_ind path_to_anchor_het path_to_vcf output_directory')
 
 arg_list=sys.argv
 the_chr=arg_list[1]
 anchorind=arg_list[2]
 recentind=arg_list[3]
+hetpath=arg_list[4]
+vcf_path=arg_list[5]
+outdir=arg_list[6]
 #print(the_chr)
 #print(recentind)
 #input()
 ###########################################################################################
 ###########################################################################################
 
-#Get vcf file name
-file_dict = get_file_name.get_name_file_dict()
-recentind_vcf = vcf_path+'/'+file_dict[recentind]
-vcf_in = recentind_vcf.split('.vc')
-recent_vcf = vcf_in[0] + the_chr + '.vc' + vcf_in[1]
 
 NUCL=['A','C','G','T']
 nt_set=set(NUCL)
 
-OKCHR=[]
-for i in range(22):
-    OKCHR.append(str(i+1))
-OK_CHR=set(OKCHR)
 
 #########################################
 #########################################
@@ -162,63 +183,93 @@ win_end=win_start+win_step
 print(the_chr,win_start,win_end)    #prints to slurm outfile
 count_dict[the_chr].update({(win_start,win_end): [0,0,0,0,0,0]}) # In each window total number matching ancestral, derived, and other
 
-# Begin reading HET and VCF file, ensure lines in-sink
-with open(hetpath+'/chr'+the_chr+'_'+anchorind+'_HetPos.txt','r') as het_file:
-    with gzip.open(recent_vcf,'rt',encoding='utf-8') as RecentInd:
-        l='##'
-        while l[0]=='#':
-            l=RecentInd.readline()
-        het_l=het_file.readline()
+# Begin reading HET and VCF file, ensure lines in sync
+het_file_path = hetpath
+
+with open(het_file_path, 'r') as het_file:
+    with gzip.open(vcf_path, 'rt', encoding='utf-8') as RecentInd:
+
+        # ---- Parse VCF header; find recent individual column ----
+        line = RecentInd.readline()
+        while line.startswith("##"):
+            line = RecentInd.readline()
+
+        # line is now the #CHROM header
+        header_fields = line.strip().split('\t')
+        sample_names = header_fields[9:]
+
+        if recentind not in sample_names:
+            sys.exit(f"ERROR: Recent individual '{recentind}' not found in VCF header.")
+
+        recent_idx = 9 + sample_names.index(recentind)
+
+        # Start reading data lines
+        l = RecentInd.readline()
+        het_l = het_file.readline()
+
         while l and het_l:
-            vcf_data=l.strip().split()
-            if not vcf_data[0] in OK_CHR:
-                break
-            het_d=het_l.strip().split()
-            vcf_pos=vcf_data[1]
-            het_pos=het_d[0]
+            vcf_data = l.strip().split()
+            het_d = het_l.strip().split()
+            vcf_pos = vcf_data[1]
+            het_pos = het_d[0]
 
-            while not vcf_pos==het_pos:  # hashed loop to sync two files, each of which may have missing positions
-                if int(vcf_pos) == min(int(vcf_pos), int(het_pos)):
-                    l=RecentInd.readline()
-                elif int(het_pos) == min(int(vcf_pos), int(het_pos)):
-                    het_l = het_file.readline()
-                if l and het_l:
-                    vcf_data = l.strip().split()
-                    vcf_pos = vcf_data[1]
-                    het_d = het_l.strip().split()
-                    het_pos = het_d[0]
+            # sync positions between VCF and HETPOS
+            while vcf_pos != het_pos:
+                if int(vcf_pos) < int(het_pos):
+                    l = RecentInd.readline()
                 else:
+                    het_l = het_file.readline()
+
+                if not (l and het_l):
                     break
-            if not het_pos == vcf_pos:
+
+                vcf_data = l.strip().split()
+                vcf_pos = vcf_data[1]
+                het_d = het_l.strip().split()
+                het_pos = het_d[0]
+
+            if not (l and het_l and het_pos == vcf_pos):
                 break
 
-            het_anc=het_d[1]
-            het_der=het_d[2]
-            vcf_ref=vcf_data[3]
-            vcf_alt=vcf_data[4]
-            at_pos=int(float(het_pos))
+            het_anc = het_d[1]
+            het_der = het_d[2]
+            vcf_ref = vcf_data[3]
+            vcf_alt = vcf_data[4]
+            at_pos = int(het_pos)
 
-            while at_pos>win_end: # Update dict with new window as key with empty value
-                win_start+=win_step
-                win_end+=win_step
-                print(the_chr,win_start,win_end)
-                count_dict[the_chr].update({(win_start,win_end): [0,0,0,0,0,0]})
-            qual=vcf_data[5]
-            if not qual=='.':
-#                if int(float(qual)) >= 30: # removed this threshold because biasing against low coverage sample heterozygote sites
-                flag=vcf_data[6]
-                flag_list = ['FAIL','FAIL1','FAIL2','FAIL3']
+            while at_pos > win_end:  # Update dict with new window as key with empty value
+                win_start += win_step
+                win_end += win_step
+                print(the_chr, win_start, win_end)
+                count_dict[the_chr].update({(win_start, win_end): [0, 0, 0, 0, 0, 0]})
+
+            qual = vcf_data[5]
+            if qual != '.':
+                flag = vcf_data[6]
+                flag_list = ['FAIL', 'FAIL1', 'FAIL2', 'FAIL3']
                 if flag not in flag_list:
-                    if not check_if_missingness(vcf_data):
-                        if check_if_biallelic(het_anc,het_der,vcf_ref,vcf_alt):
-                            coverage,haploid_genotype=get_haploid_genotype(vcf_data)    #all correct, extract haploid allele call
-                            match_count=orient_and_get_count_haploid(haploid_genotype,het_anc,het_der)  #Update dict based on which of ancestral, derived, other
-                            count_dict[the_chr][(win_start,win_end)]=[count_dict[the_chr][(win_start,win_end)][i] + match_count[i] for i in range(6)]
+                    format_str = vcf_data[8]
+                    sample_str = vcf_data[recent_idx]
 
-            l=RecentInd.readline()
-            het_l=het_file.readline()
+                    if not check_if_missingness(format_str, sample_str):
+                        if check_if_biallelic(het_anc, het_der, vcf_ref, vcf_alt):
+                            coverage, haploid_genotype = get_haploid_genotype(
+                                vcf_ref, vcf_alt, format_str, sample_str
+                            )
+                            # If we failed to get a usable genotype, skip
+                            if coverage > 0 and haploid_genotype in nt_set:
+                                match_count = orient_and_get_count_haploid(
+                                    haploid_genotype, het_anc, het_der
+                                )
+                                count_dict[the_chr][(win_start, win_end)] = [
+                                    count_dict[the_chr][(win_start, win_end)][i] + match_count[i]
+                                    for i in range(6)
+                                ]
 
-with open('DIR_counts_per_5cm/chr'+the_chr+'_'+anchorind+'_vs_'+recentind+'.txt','w') as outf:
+            l = RecentInd.readline()
+            het_l = het_file.readline()
+
+with open(outdir+'/'+the_chr+'_'+anchorind+'_vs_'+recentind+'.txt','w') as outf:
     for a_tuple in sorted(count_dict[the_chr].keys()):
         out_str=str(a_tuple[0])+','+str(a_tuple[1])+'\t'+make_out_str(count_dict[the_chr][a_tuple])
         outf.write(out_str+'\n')
